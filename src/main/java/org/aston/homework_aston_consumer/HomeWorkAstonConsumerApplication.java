1d0a35a59a6379b3cb85eaa50ecef691260e9dcf
Shispon <vshpakov@icloud.com>
2025-05-10 00:28:14 +0300

    Initial commit

---
+++ src/main/java/org/aston/homework_aston_consumer/HomeWorkAstonConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.aston.homework_aston_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkAstonConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkAstonConsumerApplication.class, args);
+	}
+
+}
